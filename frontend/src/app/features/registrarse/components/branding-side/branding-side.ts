30eed6436ec8f1890967ff0d4793f39413cd7caa
Sebas <jhoansecaicedo@unicauca.edu.co>
2026-05-28 09:52:50 -0500

    fix(ruta branding-side): se arregló una ruta que estaba en el branding side a local

--- frontend/src/app/features/registrarse/components/branding-side/branding-side.ts
+++ frontend/src/app/features/registrarse/components/branding-side/branding-side.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-branding-side',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './branding-side.html',
   styleUrl: './branding-side.scss',
 })
