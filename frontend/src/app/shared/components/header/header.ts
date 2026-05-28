6d41aa7395b978cefdbe7962f0b8dcc30a2c82d0
Sebas <jhoansecaicedo@unicauca.edu.co>
2026-05-28 09:41:54 -0500

    fix(sidebar): arreglé problema de desplazamiento responsivo del sidebar en móviles
     Está en proceso el agregado de mensajes de alerta en registro

--- frontend/src/app/shared/components/header/header.ts
+++ frontend/src/app/shared/components/header/header.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, computed } from '@angular/core';
+import { Component, inject, computed, effect } from '@angular/core';
 import { AuthStateService } from '../../../core/services/auth-state.service';
+import { SidebarService } from '../../../core/services/sidebar.service'; // Asegúrate de la ruta correcta
 import { KeycloakService } from 'keycloak-angular';
 
 @Component({
@@ -12,17 +13,26 @@ import { KeycloakService } from 'keycloak-angular';
 export class HeaderComponent {
   private keycloak = inject(KeycloakService);
   public authService = inject(AuthStateService);
+  public sidebarService = inject(SidebarService); // <-- Inyectamos el servicio aquí
 
-  // Obtenemos el nombre real del token
+  constructor() {
+  effect(() => {
+    console.log("¿El sidebar está abierto?:", this.sidebarService.isSidebarOpen());
+  });
+}
   public username = computed(() => {
     const profile = this.keycloak.getKeycloakInstance().profile;
     return profile ? `${profile.firstName} ${profile.lastName}` : 'Usuario';
   });
   
-  // Obtenemos el rol principal para mostrar bajo el nombre
   public mainRole = computed(() => {
     if (this.authService.isAdmin()) return 'Administrador';
     if (this.authService.isEspecialista()) return 'Especialista';
     return 'Paciente';
   });
+
+  alternarMenu() {
+    console.log('¡Clic detectado en el botón de hamburguesa del Header!');
+    this.sidebarService.toggleSidebar();
+  }
 }
